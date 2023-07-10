9b098c27b242e179f1819bd56e7789683d46da45
Ewa Zwierzyńska <e.m.zwierzynska@gmail.com>
2023-07-10 23:44:45 +0200

    initial commit

---
+++ src/main/java/lotto/Game.java
@@ -0,0 +1,17 @@
+package lotto;
+/*
+Zadanie dodatkowe (wykorzystujące generalnie wiedzę dotychczas poznaną, niekoniecznie exceptions):
+
+Napisz klasę realizującą system lotto.
+Klasa powinna zawierać pola takie jak:
+- ostatnie losowanie (tablica liczb)
+- liczba wygranych
+- metodę do wykonania losowania (generowanie losowych 6 liczb z przedziału 1-49)
+- metoda powinna przyjmować na wejściu zestaw 6 liczb i sprawdzać liczbę trafień,
+jeśli ktoś trafił 6 zwiększamy licznik wygranych
+
+Zadbaj o odpowiedni dostęp do informacji o ostatnim losowaniu.
+Dodaj getter metodę o wygrywająych liczbach zwracającą niemodyfikowalną listę (List.of()).
+ */
+public class Game {
+}
